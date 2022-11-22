081bf70dcdd227b59c80296fab371b863edb1c72
Elias-Kibret <eliaskibret2013n2@gmail.com>
2022-11-22 16:24:42 +0300

    landing page

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import Landing from './page/Landing'
+import styled from 'styled-components'
+const Button = styled.button`
+
+`
+
+function App() {
+
+  return (
+    <div>
+      <Landing/>
+    </div>
+  )
+}
+
+export default App
