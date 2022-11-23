b920ab71cabb2c3625679a9feab4470fa45d0a79
Elias-Kibret <eliaskibret2013n2@gmail.com>
2022-11-23 17:20:47 +0300

    DB connection

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import errorHandlerMiddleware from "./middleware/error.js";
 import notFoundMiddleware from "./middleware/not-found.js";
 import dotenv from "dotenv";
+import { connectDB } from "./DB/connect.js";
 dotenv.config();
 const app = express();
 
@@ -15,7 +16,15 @@ app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
 const PORT = process.env.PORT || 5000;
+const start = async (req, res) => {
+	try {
+		await connectDB(process.env.MONGO_URL);
+		app.listen(PORT, () => {
+			console.log(`server is running port ${PORT}`);
+		});
+	} catch (error) {
+		res.status(500).json(error);
+	}
+};
 
-app.listen(PORT, () => {
-	console.log(`server is running port ${PORT}`);
-});
+start();
