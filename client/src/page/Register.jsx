434ecd8ebb30fb4a51e7ab23d3dcbffae42b668c
Elias-Kibret <eliaskibret2013n2@gmail.com>
2022-11-23 10:30:14 +0300

    Form

--- client/src/page/Register.jsx
+++ client/src/page/Register.jsx
@@ -1,18 +1,56 @@
 import { useState, useEffect } from "react";
-const initialState = {
-	name: '',
-	email: '',
-	password: '',
-	isMember:true
-}
+import Wrapper from "../assets/wrappers/RegisterPage";
+import { Logo,FormRow } from "../components";
 
 const Register = () => {
-	const [value, setValue] = useState(initialState)
+	const initialState = {
+		name: '',
+		email: '',
+		password: '',
+		isMember:true
+	}
+	const [values, setValue] = useState(initialState)
+	const handleChange = (e) => {
+		console.log(e.target)
+	}
+	const onSubmit = (e) => {
+		e.preventDefault()
+		console.log(e.target)
+	}
 	//global state and useNavigate
 	return (
-		<>
-			<h1>Register</h1>;
-		</>
+		<div className="full-page">
+			<form className="form" onSubmit={onSubmit}>
+				<Logo />
+				<h3>Login</h3>
+
+				{/* Name input */}
+				<FormRow  
+					type="text"
+					labelText="Name"
+					name="name"
+					value={values.name}
+					handleChange={handleChange}
+				/>
+			<FormRow  
+				type="email"
+				labelText="Email"
+				name="email"
+				value={values.email}
+				handleChange={handleChange}
+			/>
+
+               <FormRow  
+					type="password"
+					labelText="Password"
+					name="Password"
+					value={values.password}
+					handleChange={handleChange}
+				/>
+			
+				<button type="sumbit" className="btn btn-block">Submit</button>
+			</form>
+		</div>
 	);
 };
 export default Register;
