c58b10272aaaf4a81c5a38b4950c6b40ab59c922
Elias-Kibret <eliaskibret2013n2@gmail.com>
2022-11-30 18:04:45 +0300

    dashboard

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,45 @@
+import { useState } from 'react'
 import Wrapper from '../assets/wrappers/Navbar'
+import {  FaAlignLeft, FaUserCircle, FaCaretDown } from 'react-icons/fa'
+import { useAppContext } from '../context/appContext'
+import Logo from './Logo'
 const Navbar = () => {
     return (
       <Wrapper>
             
-          <div>Navbar</div>
+            <div className="nav-center">
+                <button
+                    type="button"
+                    className="toggle-btn"
+                    onClick={() => console.log('toggled')}
+                >
+                    <FaAlignLeft />
+                </button>
+                    <div>
+                        <Logo />
+                        <h3 className="logo-texty">Dashboard</h3>
+                    </div>
+                <div className='btn-container'>
+                    <button
+                        type="button"
+                        className='btn'
+                        onClick={()=>{console.log('show/hide')}}
+                    >
+                        <FaUserCircle />
+                        john
+                        <FaCaretDown/>
+                    </button>
+                    <div className="dropdown show-dropdown">
+                        <button
+                            type="button"
+                            className='dropdown-btn'
+                            onClick={()=>console.log('logout')}
+                        >
+                         logout
+                        </button>
+                    </div>
+                    </div>
+         </div>
       </Wrapper>
   )
 }
