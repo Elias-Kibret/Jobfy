098412c8a7f266092a8466402fa39e295100951e
Elias-Kibret <eliaskibret2013n2@gmail.com>
2022-12-07 22:07:09 +0300

    get all jobs

--- server/Controller/jobController.js
+++ server/Controller/jobController.js
@@ -1,15 +1,18 @@
+import job from "../Model/JobModel.js";
 import jobModel from "../Model/JobModel.js";
 export const createJob = async (req, res) => {
 	const { company, position, status, jobType, jobLocation } = req.body;
-	console.log(req.user);
-	// const newJob = await jobModel({
-	// 	position,
-	// 	company,
-	// 	status,
-	// 	jobLocation,
-	// 	jobType,
-	// }).save();
-	// res.status(200).json(newJob);
+
+	console.log(req.body);
+	const newJob = await jobModel({
+		position,
+		company,
+		status,
+		jobLocation,
+		jobType,
+		createdBy: req.user,
+	}).save();
+	res.status(200).json(newJob);
 };
 export const deleteJob = async (req, res) => {
 	try {
@@ -19,11 +22,7 @@ export const deleteJob = async (req, res) => {
 	}
 };
 export const getAllJobs = async (req, res) => {
-	try {
-		res.status(200).json("successfull");
-	} catch (error) {
-		res.status(500).json(error);
-	}
+	const jobs = await find;
 };
 export const updateJob = async (req, res) => {
 	try {
