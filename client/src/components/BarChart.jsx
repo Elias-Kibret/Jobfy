436f9d312068ba91195bb97e557db7460cbfde86
Elias-Kibret <eliaskibret2013n2@gmail.com>
2022-12-09 10:12:24 +0300

    zupi

--- client/src/components/BarChart.jsx
+++ client/src/components/BarChart.jsx
@@ -1,6 +1,15 @@
-const BarChart = () => {
+import {BarChart,Bar,XAxis,YAxis,CartesianGrid,Tooltip,ResponsiveContainer} from 'recharts'
+const BarChartComponent = ({data}) => {
   return (
-    <div>BarChart</div>
+      <ResponsiveContainer>
+          <BarChart data={data} margin={{ tiop: 50 }}>
+              <CartesianGrid strokeDasharray='3 3' />
+              <XAxis dataKey='date' />
+              <YAxis allowDecimals={false} />
+              <Tooltip />
+              <Bar dataKey={data.count} fill='#2cb1bc' barSize={75}/>
+          </BarChart>
+    </ResponsiveContainer>
   )
 }
-export default BarChart
+export default BarChartComponent
