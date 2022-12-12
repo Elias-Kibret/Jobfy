9ea6fc96b7fa4663858fa8cb687bb3ebb4cb75d2
Elias-Kibret <eliaskibret2013n2@gmail.com>
2022-12-12 12:40:27 +0300

    update

--- client/src/page/DashBoard/Profile.jsx
+++ client/src/page/DashBoard/Profile.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { FormRow, Alert } from "../../components";
 import { useAppContext } from "../../context/appContext";
 import Wrapper from '../../assets/wrappers/DashboardFormPage'
-import e from "cors";
+
 
 const Profile = () => {
 	const { user, showAlert, displayAlert, updateUser, isLoading } = useAppContext()
